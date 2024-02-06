3add2fce87ba5a2071426ae691457ecf070a3d76
ysebasst <ysebasst@gmail.com>
2024-02-06 17:00:35 +0000

    cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path')
+const cors = require('cors')
 
 const app = express();
 const port = process.argv[3] || 3000;
+
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
